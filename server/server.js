2f3406ec4ac0c34b329880dc13333ef226c98fdd
felixlaii <felix.lai@hotmail.com>
2021-11-28 16:02:43 -0500

    set up basic curl test

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const usersRoute = require('./routes/users')
+const knex = require('knex')(require('./knexfile').development);
 // const petRoute = require('./routes/petRoute')
 const cors = require('cors')
 
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/users", usersRoute);
+app.get('/users', (req, res) => {
+  res.send('Welcome to my API');
+});
 // app.use("/petRoute", petRoute);
 
 app.listen(PORT, () => {
