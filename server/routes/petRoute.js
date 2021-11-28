df2ef3edcc9edbca843e3703492f4f642d6aeb72
felixlaii <felix.lai@hotmail.com>
2021-11-28 10:46:24 -0500

    created pet route

--- server/routes/petRoute.js
+++ server/routes/petRoute.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const jwt = require("jsonwebtoken");
+const authorize = require("../middleware/authorize");
+
+router.get('/v2/animals', authorize, (req, res) => {
+   
+
+})
 
 
 module.exports = router;
