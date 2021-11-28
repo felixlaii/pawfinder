58734a29b104376bd4c5f4344cb019824f4847f7
felixlaii <felix.lai@hotmail.com>
2021-11-27 19:05:32 -0500

    created tables and seeded information

---
+++ server/seeds/seed_data/user_data.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    userId: 1,
+    firstName: 'Felix',
+    lastName: 'Lai',
+    username: 'felixlaii',
+    password: 'Kering225',
+    searchRadius: '20KM',
+    animalType: 'Dog',
+    breedType: 'Corgi',
+    age: '2',
+  }
+];
