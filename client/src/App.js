4c2d18cc5b58ef9b1d376537f2491874638da5d7
felixlaii <felix.lai@hotmail.com>
2021-11-28 12:01:32 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/login" component={Login} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
