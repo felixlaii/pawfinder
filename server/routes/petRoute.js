a015b3d12c1898bd1536853a099d1a6f1f358a24
felixlaii <felix.lai@hotmail.com>
2021-12-02 11:24:45 -0500

    attempting refresh token in db

--- server/routes/petRoute.js
+++ server/routes/petRoute.js
@@ -1,18 +1,17 @@
 const router = require("express").Router();
 const axios = require("axios");
-// const knex = require('knex')(require('../knexfile').development);
+const knex = require('knex')(require('../knexfile').development);
 
-require("dotenv").config();
-const BEARER_TOKEN = process.env.BEARER_TOKEN
+// require("dotenv").config();
+// const BEARER_TOKEN = process.env.BEARER_TOKEN
 
 router.get("/", (req, res) => {
-  // knex.select("*").from("token").orderBy("id", "desc").limit(1)
-  // .then((data) => {
-      // console.log(data[0].auth_token)
+  knex.select("*").from("token").orderBy("id", "desc").limit(1)
+  .then((data) => {
       axios
         .get("https://api.petfinder.com/v2/animals", {
           headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
+            Authorization: `Bearer ${data.auth_token}`,
           },
         })
         .then((response) => {
@@ -21,31 +20,31 @@ router.get("/", (req, res) => {
         // .catch((error) => (console.log(data)))
         .catch((error) => res.status(400).send({ message: error }));
     });
-// });
+});
 
-// router.get("/organizations", (req, res) => {
-//   knex.select("*").from("token").orderBy("id", "desc").limit(1)
-//     .then((data) => {
-//       axios
-//         .get("https://api.petfinder.com/v2/organizations", {
-//           headers: {
-//             Authorization: `Bearer ${data.auth_token}`,
-//           },
-//         })
-//         .then((response) => {
-//           res.status(200).json(response.data);
-//         })
-//         .catch((error) => res.status(400).send({ message: error }));
-//     })
-// });
+router.get("/organizations", (req, res) => {
+  knex.select("*").from("token").orderBy("id", "desc").limit(1)
+    .then((data) => {
+      axios
+        .get("https://api.petfinder.com/v2/organizations", {
+          headers: {
+            Authorization: `Bearer ${data.auth_token}`,
+          },
+        })
+        .then((response) => {
+          res.status(200).json(response.data);
+        })
+        .catch((error) => res.status(400).send({ message: error }));
+    })
+});
 
 router.get("/petpreference/:species/", (req, res) => {
-  // knex.select("*").from("token").orderBy("id", "desc").limit(1)
-    // .then((data) => {
+  knex.select("*").from("token").orderBy("id", "desc").limit(1)
+    .then((data) => {
       axios
         .get("https://api.petfinder.com/v2/animals", {
           headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
+            Authorization: `Bearer ${data.auth_token}`,
           },
         })
         .then((response) => {
@@ -56,7 +55,7 @@ router.get("/petpreference/:species/", (req, res) => {
           res.status(200).json(filteredAnimals);
         })
         .catch((error) => res.status(400).send({ message: error }));
-    // })
+    })
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const usersRoute = require('./routes/usersRoute')
-const petRoute = require('./routes/petRoute')
-const searchRoute = require('./routes/searchRoute')
-const knex = require('knex')(require('./knexfile').development);
-const cors = require('cors')
-const axios = require('axios');
-const { query } = require('express');
+const usersRoute = require("./routes/usersRoute");
+const petRoute = require("./routes/petRoute");
+const searchRoute = require("./routes/searchRoute");
+const knex = require("knex")(require("./knexfile").development);
+const cors = require("cors");
+const axios = require("axios");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
@@ -14,43 +13,48 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// app.use('/', (req, res, next) => {
-//   // request token and save into db
-//   // save token and the time it was created
-//   // token expires in 1 hour - check time to ensure it is still less than an hr. 
-//   // if so, use token that exists, if not, create new token. 
-
-//   knex.select('*').from('token').orderBy('id', 'desc').limit(1)
-//     .then(data => {
-//       let currentTime = Date.now() 
-//       if(currentTime - data.created_at < 3600000){
-//         axios.post('https://api.petfinder.com/v2/oauth2/token', "grant_type=client_credentials&client_id=Irlgp1H9i7TVwwg75zoLXde1qwqUqtm5H0HkwsfiDuFpbVVSnG&client_secret=WBsI6pxWtf33UEyVAFKkailwuN1dyN8ssVVqU4Um", {
-//           headers: {
-//               "Content-Type": "application/x-www-form-urlencoded"
-//           }
-//       }).then(response => {
-      
-//         console.log("hello", response.data)
-//         knex('token').insert({
-//           auth_token: response.data.access_token,
-//           created_at: Date.now().toString()
-//         }).then(result => {
-//           console.log(result)
-//         }).catch(error => (console.log(error)))
-//       }) 
-
-//       }
-//       console.log(data.created_at)
-//     })
-// next()
-// })
+app.use("/", (req, res, next) => {
+  knex
+    .select("*")
+    .from("token")
+    .orderBy("id", "desc")
+    .limit(1)
+    .then((data) => {
+      data = data[0];
+      let currentTime = Date.now();
+      if (!data || currentTime - data.created_at >= 3600000) {
+        axios
+          .post(
+            "https://api.petfinder.com/v2/oauth2/token",
+            "grant_type=client_credentials&client_id=Irlgp1H9i7TVwwg75zoLXde1qwqUqtm5H0HkwsfiDuFpbVVSnG&client_secret=WBsI6pxWtf33UEyVAFKkailwuN1dyN8ssVVqU4Um",
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+            }
+          )
+          .then((response) => {
+            knex("token")
+              .insert({
+                auth_token: response.data.access_token,
+                created_at: Date.now().toString(),
+              })
+              .then((result) => {
+                console.log(result);
+              })
+              .catch((error) => console.log(error));
+          });
+      }
+    });
+  next();
+});
 app.use("/users", usersRoute);
-app.use('/', petRoute)
-app.use('/search', searchRoute);
-app.get('/users', (req, res) => {
-  res.send('Welcome to my API');
+app.use("/", petRoute);
+app.use("/search", searchRoute);
+app.get("/users", (req, res) => {
+  res.send("Welcome to my API");
 });
 
 app.listen(PORT, () => {
-    console.log(`${PORT}`);
-  });
+  console.log(`${PORT}`);
+});
