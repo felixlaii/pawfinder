1499d0de81a8e0a14417957a687da7611eb9584f
felixlaii <felix.lai@hotmail.com>
2021-12-05 15:20:55 -0500

    search route set up for age and breed

--- server/routes/searchRoute.js
+++ server/routes/searchRoute.js
@@ -2,55 +2,36 @@ const router = require("express").Router();
 const axios = require("axios");
 const knex = require('knex')(require('../knexfile').development);
 
-router.get("/search/:searchquery", (req, res) => {
+router.get("/search/:species/:age/:breeds", (req, res) => {
   knex.select("*").from("token").orderBy("id", "desc").limit(1)
   .then((data) => {
       data = data[0];
-    //   console.log(data[0].auth_token)
       axios
       .get("https://api.petfinder.com/v2/animals", {
           headers: {
               Authorization: `Bearer ${data.auth_token}`,
             },
         })
-        .then((response) => {
+        .then((response) => { 
             let animals = response.data.animals
-            let filteredAnimals = animals.filter((animal) => {
-                
-                return animal.species.toLowerCase().includes(req.params.searchquery)
-            })                
-            res.status(200).json(filteredAnimals);
+            let filteredAnimals = animals.filter((animal) => { 
+                return animal.species.toLowerCase().includes(req.params.species)
+            })
+           
+            let filteredAge = filteredAnimals.filter((animal) => {
+                return animal.age.toLowerCase().includes(req.params.age)
+            })
+
+            let filteredBreeds = filteredAnimals.filter((animal) => {
+                return animal.breeds.primary.toLowerCase().includes(req.params.breeds)
+            })
+                         
+            res.status(200).json(filteredAnimals)
+            res.status(200).json(filteredAge)
+            res.status(200).json(filteredBreeds)
         })
         .catch((error) => res.status(400).send([]));
     });
 });
 
-router.get("/search/:searchquery", (req, res) => {
-  knex.select("*").from("token").orderBy("id", "desc").limit(1)
-  .then((data) => {
-      data = data[0];
-      axios
-      .get("https://api.petfinder.com/v2/animals", {
-          headers: {
-              Authorization: `Bearer ${data.auth_token}`,
-            },
-        })
-        .then((response) => {
-            let animals = response.data.animals
-            let filteredAge = animals.age.filter((animals) => {
-                return animal.age.toLowerCase().includes(req.params.searchQuery)
-            })                
-            res.status(200).json(filteredAge);
-        })
-        .catch((error) => res.status(400).send({ message: error }));
-    });
-});
-
 module.exports = router;
-
-// .then((response) => {
-//     let filteredAge = animals.filter((animals) => {
-//         return animal.age.toLowerCase().includes(req.params.searchQuery)
-//     })
-//     res.status(200).json(filteredAge)
-// })
